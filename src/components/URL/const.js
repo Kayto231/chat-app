3423e5e6fe07689dc3c945dee34366df6906b8f9
Alex <oleksiym81@icloud.com>
2022-06-02 19:24:35 +0300

    Changed URLS

--- src/components/URL/const.js
+++ src/components/URL/const.js
@@ -1,30 +1,33 @@
-// export const loginUrl = "https://alex-chat-app-e.herokuapp.com/auth/login";
-// export const registerUrl =
-//   "https://alex-chat-app-e.herokuapp.com/auth/register";
-// export const authUrl = "https://alex-chat-app-e.herokuapp.com/auth/";
+export const loginUrl = "https://alex-chat-app-e.herokuapp.com/auth/login";
+export const registerUrl =
+  "https://alex-chat-app-e.herokuapp.com/auth/register";
+export const authUrl = "https://alex-chat-app-e.herokuapp.com/auth/";
 
-// export const getMessagesUrl =
-//   "https://alex-chat-app-e.herokuapp.com/chat/getmessages";
+export const getMessagesUrl =
+  "https://alex-chat-app-e.herokuapp.com/chat/getmessages";
 
-// export const socketConnectionUrl = "wss://socketio-alex-chat.herokuapp.com/";
+export const socketConnectionUrl = "wss://socketio-alex-chat.herokuapp.com/";
 
-// export const getconversations =
-//   "https://alex-chat-app-e.herokuapp.com/chat/getconversations";
+export const getconversations =
+  "https://alex-chat-app-e.herokuapp.com/chat/getconversations";
 
-// export const createNewMessage =
-//   "https://alex-chat-app-e.herokuapp.com/chat/createnewmessage";
+export const createNewMessage =
+  "https://alex-chat-app-e.herokuapp.com/chat/createnewmessage";
 
-export const loginUrl = "http://localhost:5000/auth/login";
-export const registerUrl = "http://localhost:5000/auth/register";
-export const authUrl = "http://localhost:5000/auth/";
+export const getAllUsersUrl =
+  "http://alex-chat-app-e.herokuapp.com/chat/getusers";
 
-export const getMessagesUrl = "http://localhost:5000/chat/getmessages";
-export const getconversations = "http://localhost:5000/chat/getconversations";
+// export const loginUrl = "http://localhost:5000/auth/login";
+// export const registerUrl = "http://localhost:5000/auth/register";
+// export const authUrl = "http://localhost:5000/auth/";
 
-export const createNewMessage = "http://localhost:5000/chat/createnewmessage";
-export const createNewConversationUrl =
-  "http://localhost:5000/chat/createconversation";
+// export const getMessagesUrl = "http://localhost:5000/chat/getmessages";
+// export const getconversations = "http://localhost:5000/chat/getconversations";
 
-export const socketConnectionUrl = "ws://localhost:4001/";
+// export const createNewMessage = "http://localhost:5000/chat/createnewmessage";
+// export const createNewConversationUrl =
+//   "http://localhost:5000/chat/createconversation";
 
-export const getAllUsersUrl = "http://localhost:5000/chat/getusers";
+// export const socketConnectionUrl = "ws://localhost:4001/";
+
+// export const getAllUsersUrl = "http://localhost:5000/chat/getusers";
