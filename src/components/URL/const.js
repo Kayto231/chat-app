8473805b2a4ec30ecbd256de5a3beea3ac8b1dfc
Alex <oleksiym81@icloud.com>
2022-06-02 19:40:20 +0300

    URLS

--- src/components/URL/const.js
+++ src/components/URL/const.js
@@ -1,21 +1,24 @@
-// export const loginUrl = "https://alex-chat-app-e.herokuapp.com/auth/login";
-// export const registerUrl =
-//   "https://alex-chat-app-e.herokuapp.com/auth/register";
-// export const authUrl = "https://alex-chat-app-e.herokuapp.com/auth/";
+export const loginUrl = "https://alex-chat-app-e.herokuapp.com/auth/login";
+export const registerUrl =
+  "https://alex-chat-app-e.herokuapp.com/auth/register";
+export const authUrl = "https://alex-chat-app-e.herokuapp.com/auth/";
 
-// export const getMessagesUrl =
-//   "https://alex-chat-app-e.herokuapp.com/chat/getmessages";
+export const getMessagesUrl =
+  "https://alex-chat-app-e.herokuapp.com/chat/getmessages";
 
-// export const socketConnectionUrl = "wss://socketio-alex-chat.herokuapp.com/";
+export const socketConnectionUrl = "wss://socketio-alex-chat.herokuapp.com/";
 
-// export const getconversations =
-//   "https://alex-chat-app-e.herokuapp.com/chat/getconversations";
+export const getconversations =
+  "https://alex-chat-app-e.herokuapp.com/chat/getconversations";
 
-// export const createNewMessage =
-//   "https://alex-chat-app-e.herokuapp.com/chat/createnewmessage";
+export const createNewMessage =
+  "https://alex-chat-app-e.herokuapp.com/chat/createnewmessage";
 
-// export const getAllUsersUrl =
-//   "http://alex-chat-app-e.herokuapp.com/chat/getusers";
+export const getAllUsersUrl =
+  "http://alex-chat-app-e.herokuapp.com/chat/getusers";
+
+export const createNewConversationUrl =
+  "http://alex-chat-app-e.herokuapp.com/chat/createconversation";
 
 // export const loginUrl = "http://localhost:5000/auth/login";
 // export const registerUrl = "http://localhost:5000/auth/register";
